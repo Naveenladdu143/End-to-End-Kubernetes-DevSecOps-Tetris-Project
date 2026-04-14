b78d9f3346543f5aa290614d40ff575fabb33c21
Naveen lingampalli <164299466+Naveenladdu143@users.noreply.github.com>
2026-04-14 15:19:26 +0530

    Update eks-cluster.tf

--- EKS-TF/eks-cluster.tf
+++ EKS-TF/eks-cluster.tf
@@ -1,12 +1,19 @@
-resource "aws_eks_cluster" "eks-cluster" {
-  name     = var.cluster-name
+resource "aws_eks_cluster" "eks_cluster" {
+  name     = var.cluster_name
   role_arn = aws_iam_role.EKSClusterRole.arn
+
   vpc_config {
-    subnet_ids         = [data.aws_subnet.subnet.id, aws_subnet.public-subnet2.id]
-    security_group_ids = [data.aws_security_group.sg-default.id]
+    subnet_ids = aws_subnet.private[*].id   # ✅ ONLY private subnets
+
+    security_group_ids = [data.aws_security_group.sg_default.id]
+
+    endpoint_private_access = true
+    endpoint_public_access  = true
   }
 
-  version = 1.28
+  version = "1.28"
 
-  depends_on = [aws_iam_role_policy_attachment.AmazonEKSClusterPolicy]
+  depends_on = [
+    aws_iam_role_policy_attachment.AmazonEKSClusterPolicy
+  ]
 }
